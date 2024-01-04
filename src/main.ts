cf4f87a8e20cf66980fc79d9e9da602e78dd33f7
Janne Keipert <jannekeipert@gmx.de>
2024-01-04 22:39:15 +0100

    Test for new main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,39 @@
-import "@motion-canvas/player";
+import {Stage, Player} from '@motion-canvas/core';
+import project from '../public/animations/project.js';
 
-(function prependBase() {
-  const base = import.meta.env.BASE_URL;
-  if (!base) {
-    return;
-  }
-  document.querySelectorAll("motion-canvas-player").forEach((player) => {
-    let url = player.getAttribute("src");
-    if (url?.startsWith("/")) {
-      url = base + url.slice(1);
-      const newElement = document.createElement("motion-canvas-player");
-      newElement.setAttribute("auto", player.getAttribute("auto") ?? "true");
-      newElement.setAttribute("src", url);
-      player.replaceWith(newElement);
-    }
-  });
-})();
+const stage = new Stage();
+stage.configure(project.meta.getFullRenderingSettings());
+document.body.append(stage.finalBuffer);
+
+const player = new Player(project);
+player.onRender.subscribe(async () => {
+  stage.render(
+    player.playback.currentScene,
+    player.playback.previousScene,
+  );
+});
+
+
+
+
+// import "@motion-canvas/player";
+
+// (function prependBase() {
+//   const base = import.meta.env.BASE_URL;
+//   if (!base) {
+//     return;
+//   }
+//   document.querySelectorAll("motion-canvas-player").forEach((player) => {
+//     let url = player.getAttribute("src");
+//     if (url?.startsWith("/")) {
+//       url = base + url.slice(1);
+//       const newElement = document.createElement("motion-canvas-player");
+//       newElement.setAttribute("auto", player.getAttribute("auto") ?? "true");
+//       newElement.setAttribute("src", url);
+//       player.replaceWith(newElement);
+//     }
+//   });
+// })();
+
+
+

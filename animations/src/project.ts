84ebca0eb5d0ed0bed148b62cacbdafb1823bdf3
Janne <60547730+Janne6565@users.noreply.github.com>
2024-01-05 14:04:57 +0000

    added roboto font to styles

--- animations/src/project.ts
+++ animations/src/project.ts
@@ -5,6 +5,8 @@ import Content from './scenes/Content?scene';
 import Schluss from './scenes/Schluss?scene';
 import autoplayPlugin from './autoplayPlugin';
 
+import './global.css'; 
+
 export default makeProject({
   scenes: [einleitung, Content, Schluss],
   plugins: [autoplayPlugin()]
